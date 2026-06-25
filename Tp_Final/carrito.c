c50bf0039aa6d98863d7b435c682a075d9e320de
lenk2 <lenkybtz.b912@gmail.com>
2026-06-25 03:49:48 -0300

    libreria pila fix

--- Tp_Final/carrito.c
+++ Tp_Final/carrito.c
@@ -177,15 +177,17 @@ void gestionarPago (stItemCarrito* carrito, int val, Pila* precios)
     }
 }
 
-float sumaTotal (Pila* precios){
-float totalAPagar = 0;
-if (!pilavacia(precios)){
+float sumaTotal (Pila* precios)
+{
+    float totalAPagar = 0;
+    if (!pilavacia(precios))
+    {
 
-    float precioActual = desapilar(precios);
-    totalAPagar = precioActual + sumaTotal(precios);
-}
+        float precioActual = desapilar(precios);
+        totalAPagar = precioActual + sumaTotal(precios);
+    }
 
-return totalAPagar;
+    return totalAPagar;
 }
 
 void mostrarCarrito(stItemCarrito* carrito, int validos)
